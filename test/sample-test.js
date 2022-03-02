8ab85479e7ccc3e91f156a6884b6cd93c4a3a7a4
Abhishek Faliya <57398733+Maverick9081@users.noreply.github.com>
2022-03-02 16:51:06 +0530

    Added some extra tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -21,17 +21,32 @@ beforeEach(async function () {
 })
 
 
-describe("Stakes the nft into the contract",async function(){
+describe("Staking",async function(){
 
   it("The staked NFT is stored in the contract", async function(){
     
     const [owner, add1,add2] = await ethers.getSigners();
-    await staker.connect(add1).stakeNftFor18Months(nftAddress,5000,1);
+    await staker.connect(add1).stakeNft(nftAddress,5000,1,36);
     expect(await nft.balanceOf(stakerAddress,1)).to.equal("1");
     expect(await nft.balanceOf(add1.address,1)).to.equal("1");
   })
 
-   
+  it("after unstaking the nft the rewards and nft are tranferred to user's account",async function(){
 
+    const [owner, add1,add2] = await ethers.getSigners();
+    await staker.connect(add1).stakeNft(nftAddress,5000,1,18);
+    await staker.connect(add1).unstakeNft(1);
+    expect(await nft.balanceOf(stakerAddress,1)).to.equal("0");
+    expect(await nft.balanceOf(add1.address,1)).to.equal("2");
+    expect(await nft.balanceOf(add1.address,0)).to.equal("375")
+  })
 
+  it("If a user stakes another time then the APR is restricted to 12.5%",async function(){
+
+    const [owner, add1,add2] = await ethers.getSigners();
+    await staker.connect(add1).stakeNft(nftAddress,5000,1,18);
+    await staker.connect(add1).stakeNft(nftAddress,5000,1,18);
+    await staker.connect(add1).unstakeNft(2);
+    expect(await nft.balanceOf(add1.address,0)).to.equal("937");
+  })
 })
